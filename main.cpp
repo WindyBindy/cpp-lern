278c22a184f6e6a43e63797a1cc103da55e98b5d
WindyBindy <nikita2021novsk@gmail.com>
2025-04-07 13:48:19 +0200

    hello world!

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// my first program in C++
+#include <iostream>
+
+int main(){
+
+  // asdhf
+  // int x = 4;
+  // int y = 6;
+  // std::cout << x + y;
+  // double price = 10.9;
+  // char gardle = 'A';
+  // bool tr = true;
+  int age = 21;
+  return 0;
+}
